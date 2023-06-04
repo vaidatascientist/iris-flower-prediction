a29367521eb9fee23ddaad36f1ca2dd233fab71e
Vaibhav <is0591ie@ed.ritsumei.ac.jp>
2023-06-04 19:14:19 +0900

    updated

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ scaler = pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -21,6 +21,14 @@ def predict():
     print(new_data)
     output = int(model.predict(new_data))
     return jsonify(output)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template('index.html', prediction_text='The predicted type of flower is {}'.format(output))
 
 if __name__ == '__main__':
     app.run(debug=True)
