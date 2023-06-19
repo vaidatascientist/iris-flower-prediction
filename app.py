6f24242f86ed2eda5c104ca7a81b2ccc231008b3
Vaibhav <is0591ie@ed.ritsumei.ac.jp>
2023-06-19 16:46:09 +0900

    final

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def predict():
     return render_template('index.html', prediction_text='The predicted type of flower is {}'.format(output))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
